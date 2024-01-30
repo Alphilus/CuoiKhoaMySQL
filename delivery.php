258c2a6da5dc743f5762dd87da570f8b170f3253
Alphilus <58624681+Alphilus@users.noreply.github.com>
2024-01-30 11:10:25 +0700

    Add files via upload

---
+++ delivery.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `lastest`
+ */
+
+/* `lastest`.`delivery` */
+$delivery = array(
+  array('name' => 'Cosmic Dreamcatcher','order_quantity' => '9','order_date' => '23/11/2022','status' => 'Delivered'),
+  array('name' => 'Self-watering Plant Pot Trio','order_quantity' => '1','order_date' => '13/04/2023
+','status' => 'Delivered'),
+  array('name' => 'Pocket-sized Star Projector','order_quantity' => '2','order_date' => '28/09/2023','status' => 'Waiting Confirmation'),
+  array('name' => 'Rainbow-colored Wind Chimes','order_quantity' => '20','order_date' => '11/11/2023
+','status' => 'Cancelled'),
+  array('name' => 'Cosmic Dreamcatcher','order_quantity' => '5','order_date' => '23/07/2023','status' => 'Waiting Confirmation')
+);
